0895efed673fe527efb62dfcecbd2701bce4f253
Bruno Chaves <bruno@chaves.fr>
2024-07-29 11:11:04 +0200

    first test

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface PartsPerson extends Schema.Component {
+  collectionName: 'components_parts_people';
+  info: {
+    displayName: 'Person';
+    icon: 'bulletList';
+  };
+  attributes: {
+    first_name: Attribute.String & Attribute.Required;
+    last_name: Attribute.String & Attribute.Required;
+    status: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'parts.person': PartsPerson;
+    }
+  }
 }
