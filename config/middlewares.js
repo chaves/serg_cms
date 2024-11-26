0b9a638cd0aa01462f02a6ec51dea57cf9d245a7
Bruno Chaves <bruno@chaves.fr>
2024-11-26 14:24:14 +0100

    STRAP 5

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,7 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  //'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +9,20 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "default-src": ["'self'", "data:", "blob:"],
+          "script-src": ["'self'", "cdn.ckeditor.com"],
+          "connect-src": ["'self'", "cdn.ckeditor.com"],
+          "img-src": ["'self'", "data:", "blob:", "cdn.ckeditor.com"],
+          "style-src": ["'self'", "'unsafe-inline'", "cdn.ckeditor.com"],
+          "font-src": ["'self'", "cdn.ckeditor.com"],
+        },
+      },
+    },
+  },
 ];
